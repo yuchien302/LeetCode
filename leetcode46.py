61bd9864a29ceca3031a61230d3b867b99fbb862
yuchien <yc2257@cornell.edu>
2015-10-16 13:51:10 -0400

    Add #46

---
+++ leetcode46.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        from itertools import permutations
+        return list(permutations(nums))
